312e8f08632853730cf854a8c3a7fc5773c836c1
Mark Bonicillo <markabonicillo@gmail.com>
2022-09-09 05:18:26 -0700

    Cleanup test

--- src/pa1/TestNurseryRhyme.java
+++ src/pa1/TestNurseryRhyme.java
@@ -1,4 +1,5 @@
 package pa1;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -8,62 +9,64 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-
-
 public class TestNurseryRhyme {
-  private static final String TEST_OUTPUT = "expected_output.txt";
-  private static final String MY_OUTPUT = "actual_output.txt";
-  
-  public static void main(String args[]) throws IOException {
-    System.out.println("Output of your program...");
-    NurseryRhyme.main(null);
-    
-    System.out.println("\nStarting tests...");
+    private static final String EXPECTED_OUTPUT = "expected_output.txt";
+    private static final String ACTUAL_OUTPUT = "actual_output.txt";
+    private static final long START_IGNORE = 35;
+    private static final long END_IGNORE = 37;
 
-    //Instantiating the File class
-    File file = new File(MY_OUTPUT);
-    String parentPath = file.getAbsoluteFile().getParent();
+    public static void main(String args[]) throws IOException {
+        File fileStudentAnswer = new File(ACTUAL_OUTPUT);
+        System.out.println(fileStudentAnswer.getAbsoluteFile().getParent());
 
-    //Instantiating the PrintStream class
-//    System.out.println("From now on "+ file.getAbsolutePath()+" will be your console");
-    System.setOut(new PrintStream(file));
+        //System.out.println("From now on " + fileStudentAnswer.getAbsolutePath()+ " will be your console");
+        System.setOut(new PrintStream(fileStudentAnswer));
 
-    //Printing values to file
-    NurseryRhyme.main(null);
+        // Run the program being tested and write output to a file
+        NurseryRhyme.main(null);
 
-    // Change printstream to stdout
-    System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        // Change printstream to stdout so that we can print to the console during testing
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
 
-    // Grade the  answer
-    File fileAnswer = new File(TEST_OUTPUT);
-    filesCompareByLine(file.toPath(), fileAnswer.toPath());
-    System.out.println("Tests complete.");
-  }
-
-  public static long filesCompareByLine(Path path1, Path path2) throws IOException {
-    try (BufferedReader bf1 = Files.newBufferedReader(path1);
-        BufferedReader bf2 = Files.newBufferedReader(path2)) {
-
-      long lineNumber = 1;
-      String line1 = "", line2 = "";
-      while ((line1 = bf1.readLine()) != null) {
-        line2 = bf2.readLine();
-        System.out.println("Checking line number " + lineNumber);
-        System.out.println("Your line: " + line1);
-        System.out.println("Correct line: " + line2);
-        if (line2 == null || !line1.equals(line2)) {
-          System.out.println("Lines do not match! " + "In your myanswer.txt, check line number: " + lineNumber);
-          return lineNumber;
-        }
-        lineNumber++;
-      }
-      if (bf2.readLine() == null) {
-        System.out.println("PASSED!");
-        return -1;
-      } else {
-        System.out.println("Your answer is not correct. See line number: " + lineNumber);
-        return lineNumber;
-      }
+        // Compare the program's file to the expected answer
+        File fileAnswerKey = new File(EXPECTED_OUTPUT);
+        filesCompareByLine(fileStudentAnswer.toPath(), fileAnswerKey.toPath(), START_IGNORE, END_IGNORE);
+        System.out.println("Tests complete.");
     }
-  }
+
+    public static long filesCompareByLine(Path path1, Path path2, long start, long end) throws IOException {
+        try (BufferedReader bf1 = Files.newBufferedReader(path1); BufferedReader bf2 = Files.newBufferedReader(path2)) {
+
+            long lineNumber = 1;
+            String line1 = "", line2 = "";
+            while ((line1 = bf1.readLine()) != null) {
+
+                line2 = bf2.readLine();
+
+                System.out.println();
+                System.out.println("Checking line number " + lineNumber);
+                System.out.println("Your line: " + line1);
+                System.out.println("Correct line: " + line2);
+
+                if (lineNumber >= start && lineNumber <= end) {
+                    System.out.println("Custom line created by student. Not checking");
+                } else if (line2 == null || !line1.equals(line2)) {
+                    System.out.println("Lines do not match! " + "In your myanswer.txt, check line number: " + lineNumber);
+                    return lineNumber;
+                } else {
+                    System.out.println("Lines match! Checking next line");
+                }
+                lineNumber++;
+            }
+
+            if (bf2.readLine() == null) {
+                System.out.println();
+                System.out.println("PASSED!");
+                return -1;
+            } else {
+                System.out.println("Your answer is not correct. See line number: " + lineNumber);
+                return lineNumber;
+            }
+        }
+    }
 }
